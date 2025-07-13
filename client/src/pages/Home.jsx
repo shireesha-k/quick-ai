91a5d2b2f9d37519fcd366a195a25dd8a6c8da48
katravth shireesha <katravaththrisha@gmail.com>
2025-07-13 19:21:29 +0530

    Plans page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import AiTools from '../components/AiTools';
 import Testimonial from '../components/Testimonial';
+import Plan from '../components/Plan';
 const Home =()=>{
     return(
        <>
@@ -10,6 +11,7 @@ const Home =()=>{
        <Hero />
        <AiTools />
        <Testimonial />
+       <Plan />
        </>
     )
 }
