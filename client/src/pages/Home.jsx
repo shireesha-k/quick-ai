fdb6e506a2284137e30e069a7600359bd223bc3b
katravth shireesha <katravaththrisha@gmail.com>
2025-07-13 12:13:30 +0530

    AITools page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import AiTools from '../components/AiTools';
 const Home =()=>{
     return(
        <>
        <Navbar />
        <Hero />
+       <AiTools />
        </>
     )
 }
